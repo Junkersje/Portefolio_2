08b0486584ee7ec50bce463dab42dd5ae547731b
Simon <simonffsss@gmail.com>
2024-09-28 21:58:51 +0200

    Before foot fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*
-*  Jeg har lavet en .js fil for hver af opgaverne i del 2 :)
+*  Jeg har lavet en mappe til javascript opgaverne :)
 *  1 - studentgradeanalysis.js
 *  2 - treasurehunter.js
 *  3 - startupnamegenerator.js
 * */
+
