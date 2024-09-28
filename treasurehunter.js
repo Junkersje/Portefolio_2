c3f23999965584bc351e9fe631d794a3bcdc477a
Simon <simonffsss@gmail.com>
2024-09-28 17:22:42 +0200

    Treasure hunter completed

---
+++ treasurehunter.js
@@ -0,0 +1,23 @@
+// Treasure hunter //
+
+// 1: Island setup
+const myIsland = ['-', '-', '-', 'T', '-', 'T']
+
+// 1.1 How many moves on the island
+const movesOnIsland = myIsland.length;
+console.log('There are ' + movesOnIsland + ' moves on the island')
+
+// 1.2 log each move
+for (let move = 0; move < movesOnIsland; move++) {
+    if(myIsland[move] === 'T'){
+        console.log('Treasure found at move ' + move + ' :)')
+    } else {
+        console.log('No treasure found :(')
+    }
+}
+
+// 1.3 log how many treasures there are on the island
+const numberOfTreasuresOnIsland = myIsland.filter(move => move === 'T').length
+console.log(numberOfTreasuresOnIsland)
+
+
